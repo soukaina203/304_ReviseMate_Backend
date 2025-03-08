9ce013a00a221e7a3d493685ca09b514c741e2aa
samenthalo <ortega.samantha81400@gmail.com>
2025-03-08 20:33:26 +0100

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { QuizModule } from './features/quiz/quiz.module';
 import { QuestionModule } from './features/question/question.module';
 import { UserModule } from './features/user/user.module';
 import { ClasseModule } from './features/classe/classe.module';
+import { SummaryModule } from './summary/summary.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { ClasseModule } from './features/classe/classe.module';
     QuestionModule, // CRUD
     UserModule,
     ClasseModule,
+    SummaryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
