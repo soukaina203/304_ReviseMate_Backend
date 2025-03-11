dcca4d8cf3eb1762c27fa0614fa922aca42a5318
samenthalo <ortega.samantha81400@gmail.com>
2025-03-11 15:17:29 +0100

    add pdf IA

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Body, Post, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as pdfParse from 'pdf-parse';
 import { SummaryService } from './summary.service';
 
 @Controller('summary')
 export class SummaryController {
   constructor(private readonly summaryService: SummaryService) {}
 
-  // The getSummary method receives the text to summarize in the request body
-  // and returns the summarized text in the response
   @Post() 
   async getSummary(@Body('text') text: string) {
     return { summary: await this.summaryService.summarizeText(text) };
+  }
+
+  @Post('pdf')
+  @UseInterceptors(FileInterceptor('file')) 
+  async getSummaryFromPdf(@UploadedFile() file: any) {  
+    // Check if a file was provided
+    if (!file) {
+      throw new BadRequestException('Aucun fichier fourni.');
+    }
+    // Check if the file is a PDF
+    try {
+      const pdfText = await pdfParse(file.buffer);
+      const summary = await this.summaryService.summarizeText(pdfText.text);
+      return { summary };
+    } catch (error) {
+      throw new BadRequestException('Erreur lors du traitement du fichier PDF.');
+    }
   }
 }
