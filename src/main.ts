96cc2aaa11ff7ca2bae4e81661e8fb252d67bf4a
soukaina203 <soukainamourabit9@gmail.com>
2025-08-26 17:41:01 +0100

    fix : remove the slash

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    // Enable CORS
    app.enableCors({
-    origin:'https://revisemate.cloud.dev-solus.com/', // Allow requests from your Angular app
+    origin:'https://revisemate.cloud.dev-solus.com', // Allow requests from your Angular app
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies and credentials
   });
